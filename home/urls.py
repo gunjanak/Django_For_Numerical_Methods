88306d5ed3a7109d18a90ebe0b8ade415d016f3e
gunjanak <janak.klal@gmail.com>
2025-01-19 19:26:09 +0545

    home page changed

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import home,home_Numerical_methods
 urlpatterns = [
     path('',home,name='home'),
+    path('nm',home_Numerical_methods,name="home_nm"),
 ]
